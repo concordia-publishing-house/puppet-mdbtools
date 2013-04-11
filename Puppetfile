5232cf281ac0a917960dc3213667eb53561d53da
Gene Doyel <gene.doyel@cph.org>
2013-04-11 12:32:53 -0500

    Initial commit

---
+++ Puppetfile
@@ -0,0 +1,7 @@
+def github(name, version, options = nil)
+  options ||= {}
+  options[:repo] ||= "boxen/puppet-#{name}"
+  mod name, version, :github_tarball => options[:repo]
+end
+
+github "homebrew", "1.1.2"
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,15 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Examples
+#
+#  include mbdtools
+
+class mdbtools {
+  exec { "install glib":
+    command => "brew boxen-install",
+    before  => Package["mdbtools"],
+  }
+  
+  package { 'mdbtools':
+    ensure   => installed,
+    provider => homebrew,
+  }
 }
