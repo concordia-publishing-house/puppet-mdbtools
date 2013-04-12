356c55a7fa444ce955abed58f673f3be4a25793d
Kendall Park <kendall.park@cph.org>
2013-04-12 13:31:02 -0500

    added require for libffi and changed the working dir for exec

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,10 +4,13 @@
 
 class mdbtools {
   require homebrew
+  require libffi
+  include boxen::config
   
   exec { "install glib":
     command => "brew boxen-install glib",
     before  => Package["mdbtools"],
+    cwd     => ${boxen::config::home}
   }
   
   package { 'mdbtools':
