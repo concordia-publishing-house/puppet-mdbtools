c3e65e3d8da04f4db7144180f7bc21935836032a
Kendall Park <kendall.park@cph.org>
2013-04-15 15:32:41 -0500

    Had to fit init.pp file

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,13 +4,13 @@
 
 class mdbtools {
   require homebrew
-  require libffi
   include boxen::config
 
   # fixes libffi dependency by rolling back to v3.0.11
   package { 'https://raw.github.com/mxcl/homebrew/d1319dfa0662af5bb18cf530061adf64cc59c349/Library/Formula/libffi.rb':
     ensure   => installed,
     provider => homebrew,
+    before   => Package['mdbtools'],
   }
   
   package { 'mdbtools':
