92ce776994a5a0e7459d8b5b8ceb02f756e61ebe
Kendall Park <kendall.park@cph.org>
2013-04-18 13:03:16 -0500

    requires a brew update before this can install

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,13 +4,14 @@
 
 class mdbtools {
   require homebrew
+  include homebrew::repo
   include boxen::config
 
   # fixes libffi dependency by patching the libffi.rb formula
   exec { 'git pull git@github.com:KendallPark/homebrew.git deparallelize-libffi':
     cwd    => "${boxen::config::homebrewdir}",
     before => Package['mdbtools'],
-    require => Exec['install-homebrew'],
+    require => Exec['brew update'],
   }
   
   package { 'mdbtools':
