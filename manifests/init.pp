f71413f6c678747366485c3670d1a966c4ec3f71
Kendall Park <kendall.park@cph.org>
2013-04-30 08:18:31 -0500

    thanks to another approved pull request into the homebrew repo, don't need these lines anymore

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,12 +6,6 @@ class mdbtools {
   require homebrew
   include homebrew::repo
   include boxen::config
-
-  exec { 'git pull git@github.com:KendallPark/homebrew.git deparallelize-mdbtools':
-    cwd    => "${boxen::config::homebrewdir}",
-    before => Package['mdbtools'],
-    require => Exec['brew update'],
-  }
   
   package { 'mdbtools':
     ensure   => installed,
